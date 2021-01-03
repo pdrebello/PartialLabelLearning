905402600859ed94babda1c142cc3a49a085a62e
PratheekRebello <pratheekrebello99@gmail.com>
2021-01-03 22:40:23 +0530

    SelectR debugging

--- SelectR.py
+++ SelectR.py
@@ -11,13 +11,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 #import torch_optimizer as optim
 import scipy.io
-from dataset import Dataset, loadTrain
+from dataset import Dataset, loadTrain, loadTrainT
 import sys
 from IPython.core.debugger import Pdb
 import random
 import csv
+import os
 
-n_epochs = 300
+n_epochs = 1
 batch_size_train = 64
 batch_size_test = 1000
 learning_rate = 0.001
@@ -35,24 +36,46 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 vals = [[],[],[],[]]
 
 def cc_loss(output, target):
-    #print(output.shape)
-    #print(target.shape)
     batch_size = output.shape[0]
     loss = torch.bmm(output.view(output.shape[0], 1, output.shape[1]), target.view(output.shape[0], output.shape[1], 1))
-    loss = torch.log(loss)
+    loss = torch.log(loss+epsilon)
     loss = torch.sum(loss)
     loss = torch.div(loss, -batch_size)
     return loss
 
 def naive_reward(output, target):
-    #print(output.shape)
-    #print(target.shape)
     batch_size = output.shape[0]
     loss = torch.bmm(output.view(output.shape[0], 1, output.shape[1]), target.view(output.shape[0], output.shape[1], 1))
     #loss = torch.log(loss)
     loss = torch.sum(loss)
     loss = torch.div(loss, -batch_size)
     return loss
+
+def select_loss_function(p, q):
+    loss = (q*torch.log(p+epsilon)).sum(dim=1).mean()
+    return -loss
+
+def select_reward_function(p, q):
+    rew = (q*p).sum(dim=1).mean()
+    return -rew
+
+def sample_reward_function(p, q, a, mask):
+    
+    rew = (a*p).sum(dim=1) - ((1-a)*mask*p).sum(dim=1)
+    #lap = sum (log (ai*qi + (1-ai)*(1-qi)))
+    lap = a*torch.log(q+ epsilon) + (1-a)*mask*torch.log(1-q+ epsilon)
+    lap = lap.sum(dim=1)
+    #(lap * rew)
+    #lap = torch.log(lap+epsilon).sum(dim=1)
+    
+    return -torch.mean(rew * lap)
+
+def sample_loss_function(p, a):
+    
+    #loss = torch.log((a*p + (1-a)*(1-p)).sum(dim=1))
+    #
+    loss = torch.log((a*p).sum(dim=1) +epsilon)
+    return -torch.mean(loss)
 
 class Prediction_Net(nn.Module):
     def __init__(self, input_dim, output_dim):
@@ -74,123 +97,69 @@ class Prediction_Net(nn.Module):
     
     def copy(self, net2):
         self.load_state_dict(net2.state_dict())
+    
+
+class Phi_Net(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Phi_Net, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, output_dim)
+        torch.nn.init.xavier_uniform(self.fc1.weight)
+        torch.nn.init.xavier_uniform(self.fc2.weight)
+        torch.nn.init.xavier_uniform(self.fc3.weight)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+
+    def forward(self, x, targetSet, rl_technique):
+        mask = targetSet.bool()
+        
+        x = x * targetSet.long()
+        x = F.elu(self.bn1(self.fc1(x)))
+        x = F.elu(self.bn2(self.fc2(x)))
+        x = self.fc3(x)
+        x[~mask] = float('-inf')
+        if(rl_technique == "sample"):
+            x = F.sigmoid(x)
+        else:
+            x = F.softmax(x)
+        return x
+    
+    def copy(self, net2):
+        self.load_state_dict(net2.state_dict())
 
 class Selection_Net(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(Selection_Net, self).__init__()
         
-        self.fc1 = nn.Linear(input_dim, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, output_dim)
-        
-        self.fc1.requires_grad=False
-        self.fc2.requires_grad=False
-        self.fc3.requires_grad=False
-        
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
-        
-        
-        self.fc1_g = nn.Linear(output_dim*2, 512)
-        self.fc2_g = nn.Linear(512, 256)
-        self.fc3_g = nn.Linear(256, output_dim)
-        torch.nn.init.xavier_uniform(self.fc1_g.weight)
-        torch.nn.init.xavier_uniform(self.fc2_g.weight)
-        torch.nn.init.xavier_uniform(self.fc3_g.weight)
-        self.bn1_g = nn.BatchNorm1d(512)
-        self.bn2_g = nn.BatchNorm1d(256)
-        
-        #self.fc1 = nn.Linear(input_dim, output_dim)
+        self.p_net = Prediction_Net(input_dim, output_dim)
+        for param in self.p_net.parameters():
+            param.requires_grad = False
+            
+        self.phi_net = Phi_Net(output_dim, output_dim)
 
-    def forward(self, x, targetSet):
-        x = F.elu(self.bn1(self.fc1(x)))
-        x = F.elu(self.bn2(self.fc2(x)))
-        x = F.softmax(self.fc3(x))
+    def forward(self, x, targetSet, rl_technique):
+        with torch.no_grad():
+            x = self.p_net(x)
         
-        #print(x.shape)
-        x_i = x.argmax(dim = 1)
-        j = torch.arange(x.size(0)).long()
-        #print(x_i)
-        x = torch.zeros(x.shape)
-        x[j, x_i] = 1
-        
-        #print(x.shape)
-        #print(targetSet.shape)
-        inp2 = torch.cat((x, targetSet), dim=1)
-        #print(inp2.shape)
-        inp2 = F.elu(self.bn1_g(self.fc1_g(inp2)))
-        inp2 = F.elu(self.bn2_g(self.fc2_g(inp2)))
-        
-        #Pdb().set_trace()
-        inp2 = F.softmax(self.fc3_g(inp2))*targetSet
-        #print(inp2.shape)
-        #print(inp2.sum(dim=1, keepdim=True)[1].shape)
-        inp2 = inp2/inp2.sum(dim=1, keepdim=True)[1]
-        
-        
-        #x = F.softmax(self.fc1(x))
-        return x, inp2
+        x = self.phi_net(x, targetSet, rl_technique)
+        return x
 
-
-
-
-filename = "Soccer Player"
-train_dataset, test_dataset, real_train_dataset, input_dim, output_dim = loadTrain(filename+".mat")
-train_loader = torch.utils.data.DataLoader(train_dataset,
-  batch_size=batch_size_train, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset,
-  batch_size=batch_size_test, shuffle=True)
-real_train_loader = torch.utils.data.DataLoader(real_train_dataset,
-  batch_size=batch_size_train, shuffle=True)
-
-loss_function = cc_loss
-reward_function = naive_reward
-
-p_net = Prediction_Net(input_dim, output_dim)
-s_net = Selection_Net(input_dim, output_dim)
-
-p_optimizer = torch.optim.Adam(p_net.parameters())
-s_optimizer = torch.optim.Adam(s_net.parameters())
-
-def train(epoch):
+def pre_train(epoch):
     p_net.train()
-    s_net.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         
         data, target = data.to(device), target.to(device)
         
         p_optimizer.zero_grad()
-        #s_optimizer.zero_grad()
-        
         output = p_net(data)
         
-        yhat, probs = s_net(data, target)
-        #print(probs.shape)
-        m = torch.distributions.Categorical(probs)
         
-        target_choice = m.sample()
-        #print(target_choice)
-        j = torch.arange(probs.size(0)).long()
-        rl_target = torch.zeros(target.shape)
-        rl_target[j, target_choice] = 1
-        loss = loss_function(output, rl_target)
-        
-        
-        loss = loss_function(output, rl_target)
-        reward = reward_function(probs, yhat)
-        
+        loss = cc_loss(output, target)
         loss.backward()
-        #p_optimizer.step()
-        reward.backward()
+        
         p_optimizer.step()
         
-        s_net.fc1.weight.data = p_net.fc1.weight.data
-        s_net.fc1.bias.data = p_net.fc1.bias.data
-        s_net.fc2.weight.data = p_net.fc2.weight.data
-        s_net.fc2.bias.data = p_net.fc2.bias.data
-        s_net.fc3.weight.data = p_net.fc3.weight.data
-        s_net.fc3.bias.data = p_net.fc3.bias.data
-        #s_optimizer.step()
         if batch_idx % log_interval == 0:
           print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
             epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -208,12 +177,75 @@ def train(epoch):
         0, correct, len(real_train_loader.dataset),
         100. * correct / len(real_train_loader.dataset)))
 
-def test():
+
+def train(epoch, rl_technique):
+    p_net.train()
+    s_net.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        
+        data, target = data.to(device), target.to(device)
+        
+        p_optimizer.zero_grad()
+        s_optimizer.zero_grad()
+        
+        #prob output of prediction network
+        p = p_net(data)
+        q = s_net(data, target, rl_technique)
+        
+        #Pdb().set_trace()
+        if(rl_technique == 'sample'):
+            if(torch.isnan(q.sum())):
+                Pdb().set_trace()
+            with torch.no_grad():
+                m = torch.distributions.bernoulli.Bernoulli(q)
+                a = m.sample()
+            mask = target.long()
+            reward = sample_reward_function(p.detach(), q, a, mask)
+            loss = sample_loss_function(p, a)
+        else:
+            loss = select_loss_function(p, q.detach())
+            reward = select_reward_function(p.detach(), q)
+        if(torch.isnan(loss)):
+            Pdb().set_trace()
+        if(torch.isinf(loss)):
+            Pdb().set_trace()
+        loss.backward()
+        reward.backward()
+        
+        p_optimizer.step()
+        s_optimizer.step()
+        
+        
+        if batch_idx % 100 == 0:
+            s_net.p_net.copy(p_net)
+        
+        if batch_idx % log_interval == 0:
+          print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tReward: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.item(), reward.item()))
+          
+    correct = 0
+    with torch.no_grad():
+        for data, target in real_train_loader:
+            data, target = data.to(device), target.to(device)
+            output = p_net.forward(data)
+            pred = output.data.max(1, keepdim=True)[1]
+            targ_pred = target.data.max(1, keepdim=True)[1]
+            correct += pred.eq(targ_pred.data.view_as(pred)).sum()
+  
+        print('\nTrain set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        0, correct, len(real_train_loader.dataset),
+        100. * correct / len(real_train_loader.dataset)))
+        
+        vals[0].append(correct.item())
+        vals[1].append(100. * float(correct.item()) / len(real_train_loader.dataset))
+
+def test(test_data):
     p_net.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for data, target in test_data:
             data, target = data.to(device), target.to(device)
             output = p_net.forward(data)
             pred = output.data.max(1, keepdim=True)[1]
@@ -221,88 +253,86 @@ def test():
             correct += pred.eq(targ_pred.data.view_as(pred)).sum()
       
       
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(test_data.dataset)
     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
-    #f.write()
-    #f.write('Test: {}/{} ({:.0f}%)\n'.format(
-    #        test_loss, correct, len(self.test_loader.dataset),
-    #        100. * correct / len(self.test_loader.dataset)))
-    #f.write("\n")
-
-  #f.write('Train: {}/{} ({:.0f}%)\n'.format(correct, len(self.real_train_loader.dataset),
-  #  100. * correct / len(self.real_train_loader.dataset)))
-  #f.write("\n")
-  #vals[0].append(correct.item())
-  #vals[1].append(100. * float(correct.item()) / len(self.real_train_loader.dataset))
-for epoch in range(1, n_epochs + 1):
-    train(epoch)
-    test()
-"""
-for batch_idx, (data, target) in enumerate(self.train_loader):
-    data, target = data.to(device), target.to(device)
-    #print(torch.sum(target,dim=1))
-    self.optimizer.zero_grad()
-    output = network(data)
-  
-self.train()
-for batch_idx, (data, target) in enumerate(self.train_loader):
-    data, target = data.to(device), target.to(device)
-    #print(torch.sum(target,dim=1))
-    self.optimizer.zero_grad()
-    output = network(data)
-    
-    loss = loss_function(output, target)
-    
-    loss.backward()
-    self.optimizer.step()
-    if batch_idx % log_interval == 0:
-      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-        epoch, batch_idx * len(data), len(self.train_loader.dataset),
-        100. * batch_idx / len(self.train_loader), loss.item()))
-  correct = 0
-  with torch.no_grad():
-      for data, target in self.real_train_loader:
-          data, target = data.to(device), target.to(device)
-          output = self.forward(data)
-          pred = output.data.max(1, keepdim=True)[1]
-          targ_pred = target.data.max(1, keepdim=True)[1]
-          correct += pred.eq(targ_pred.data.view_as(pred)).sum()
-      
-  print('\nTrain set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    0, correct, len(self.real_train_loader.dataset),
-    100. * correct / len(self.real_train_loader.dataset)))
-  #f.write('Train: {}/{} ({:.0f}%)\n'.format(correct, len(self.real_train_loader.dataset),
-  #  100. * correct / len(self.real_train_loader.dataset)))
-  #f.write("\n")
-  vals[0].append(correct.item())
-  vals[1].append(100. * float(correct.item()) / len(self.real_train_loader.dataset))
-
-def myTest(self, loss_function, vals):
-    self.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in self.test_loader:
-            data, target = data.to(device), target.to(device)
-            output = self.forward(data)
-            pred = output.data.max(1, keepdim=True)[1]
-            targ_pred = target.data.max(1, keepdim=True)[1]
-            correct += pred.eq(targ_pred.data.view_as(pred)).sum()
-      
-      
-    test_loss /= len(self.test_loader.dataset)
-    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(self.test_loader.dataset),
-            100. * correct / len(self.test_loader.dataset)))
-    #f.write()
-    #f.write('Test: {}/{} ({:.0f}%)\n'.format(
-    #        test_loss, correct, len(self.test_loader.dataset),
-    #        100. * correct / len(self.test_loader.dataset)))
-    #f.write("\n")
+            test_loss, correct, len(test_data.dataset),
+            100. * correct / len(test_data.dataset)))
     vals[2].append(correct.item())
-    vals[3].append(100. * float(correct.item()) / len(self.test_loader.dataset))    
-
-"""  
+    vals[3].append(100. * float(correct.item()) / len(test_data.dataset))
+    return (100. * float(correct.item()) / len(test_data.dataset))
     
+
+k = 10
+
+datasets = ['Soccer Player','MSRCv2','lost','BirdSong','Yahoo! News']
+
+for tech in ["sample","select"]:
+    for filename in datasets:
+        for fold_no in range(k):
+            
+            train_dataset, test_dataset, real_train_dataset, val_dataset, input_dim, output_dim = loadTrain(filename+".mat", fold_no, k)
+            train_loader = torch.utils.data.DataLoader(train_dataset,
+              batch_size=batch_size_train, shuffle=True)
+            test_loader = torch.utils.data.DataLoader(test_dataset,
+              batch_size=batch_size_test, shuffle=True)
+            val_loader = torch.utils.data.DataLoader(val_dataset,
+              batch_size=batch_size_test, shuffle=True)
+            real_train_loader = torch.utils.data.DataLoader(real_train_dataset,
+              batch_size=batch_size_train, shuffle=True)
+            
+            vals = [[],[],[],[]]
+            
+            p_net = Prediction_Net(input_dim, output_dim)
+            s_net = Selection_Net(input_dim, output_dim)
+            
+            p_optimizer = torch.optim.Adam(p_net.parameters())
+            s_optimizer = torch.optim.Adam(s_net.parameters())
+        
+        
+            best_val = 0
+            result_filename = "results/"+filename+"/"+str("SelectR")+"/results/"+str(fold_no)+"_out.txt"
+            result_log_filename = "results/"+filename+"/"+str("SelectR")+"/logs/"+str(fold_no)+"_log.csv"
+            model_filename = "results/"+filename+"/"+str("SelectR")+"/models/"+str(fold_no)+"_best.pth"
+            
+            
+            load_pre_train = "results/"+filename+"/"+str("cc_loss")+"/models/"+str(fold_no)+"_best.pth"
+            p_net.load_state_dict(torch.load(load_pre_train))
+            s_net.p_net.copy(p_net)
+            
+            #for epoch in range(1, 1):
+            #    pre_train(epoch)
+            #    test()
+            #s_net.p_net.copy(p_net)
+        
+        
+            for epoch in range(1, n_epochs + 1):
+              train(epoch, tech)
+              val = test(val_loader)
+              
+              if(val > best_val):
+                  best_val = val
+                  os.makedirs(os.path.dirname(model_filename), exist_ok=True)
+                  torch.save(p_net.state_dict(), model_filename)
+              if((epoch%10==0) and (epoch>0)):
+                  e_model_filename = "results/"+filename+"/SelectR/models/"+str(fold_no)+"_"+str(epoch)+".pth"
+                  os.makedirs(os.path.dirname(e_model_filename), exist_ok=True)
+                  torch.save(p_net.state_dict(), e_model_filename)
+            
+            
+            p_net.load_state_dict(torch.load(model_filename))
+            train_acc = test(real_train_loader)
+            val_acc = test(val_loader)
+            test_acc = test(test_loader)
+            
+            os.makedirs(os.path.dirname(result_filename), exist_ok=True)
+            with open(result_filename,"w", newline='') as file:
+                file.write(str(train_acc)+"\n")
+                file.write(str(val_acc)+"\n")
+                file.write(str(test_acc)+"\n")
+                
+            os.makedirs(os.path.dirname(result_log_filename), exist_ok=True)
+            with open(result_log_filename,"w", newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Train Count", "Train Acc", "Test Count", "Test Acc"])
+                for i in range(len(vals[0])):
+                    writer.writerow([vals[0][i], vals[1][i], vals[2][i], vals[3][i]])
